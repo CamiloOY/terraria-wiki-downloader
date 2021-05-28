375e9a714ef358e6dbb6cdbd6c356c0db7cb2c70
ckyy1g20 <ckyy1g20@soton.ac.uk>
2021-05-28 19:52:08 +0100

    Fixed CSS and link conversion

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const exec = util.promisify(require('child_process').exec);
 	await page.goto("https://terraria.fandom.com/wiki/Special:AllPages?from=&to=&namespace=0");
 	// const handle = await fs.open(`${__dirname + (await (await page.evaluate(() => window.location.pathname)).replace(":", " "))}.html`, "w");
 	await fs.mkdir("wiki", {recursive: true});
+	const url_file_handle = await fs.open("urls.txt", "w");
 	// while((await page.$x("//a[contains(text(), \"Next page\")]")).length > 0) {
 		const pages = await page.$$(".mw-allpages-chunk li a");
 		let urls = [];
@@ -19,14 +20,17 @@ const exec = util.promisify(require('child_process').exec);
 		for(let i = 0; i < urls.length; i++) {
 			// console.log((await pages[i].getProperty("href"))._remoteObject.value);
 			await page.goto(urls[i]);
+			url_file_handle.appendFile(page.url() + "\n");
 			// await pages[i].click();
 			// console.log(page.url());
-			const {stdout, stderr} = await exec(`D: && wget --convert-links -e robots=off --adjust-extension --page-requisites --no-parent --no-clobber --restrict-file-names=windows -U \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36\" -P ${__dirname + "\\wiki"} ${page.url()}`);
-			console.log("stdout", stdout);
-			console.log("stderr", stderr);
+			
 		}
 	// 	page.$x("//a[contains(text(), \"Next page\")]")[0].click();
 	// }
+	url_file_handle.close();
+	const {stdout, stderr} = await exec(`D: && wget --convert-links -e robots=off --adjust-extension --page-requisites --no-parent --no-clobber --restrict-file-names=windows -U \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36\" --input-file=${__dirname + "\\urls.txt"} -P ${__dirname + "\\wiki"}`);
+	console.log("stdout", stdout);
+	console.log("stderr", stderr);
 	// const handle = await fs.open(__dirname + "\\wiki\\" + encodeURIComponent((await page.evaluate(() => window.location.pathname)).replace("/wiki/", "")) + ".html", "w");
 	// console.log(__dirname + "\\wiki\\" + encodeURIComponent((await page.evaluate(() => window.location.pathname)).replace("/wiki/", "")) + ".html");
 	// await handle.appendFile(await page.content());
