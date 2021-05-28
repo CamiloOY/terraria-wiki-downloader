7e85e98481f24c6e943fdad5e371efcbc144048a
ckyy1g20 <ckyy1g20@soton.ac.uk>
2021-05-28 20:40:18 +0100

    Fixed bug in going to next page of page list

--- index.js
+++ index.js
@@ -5,13 +5,15 @@ const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 
 (async () => {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({executablePath: process.env.chrome, args: [`--load-extension=C:\\Users\\Camilo\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\cjpalhdlnbpafiamejdnhcphjbkeiagm`]});
 	const page = await browser.newPage();
 	await page.goto("https://terraria.fandom.com/wiki/Special:AllPages?from=&to=&namespace=0");
 	// const handle = await fs.open(`${__dirname + (await (await page.evaluate(() => window.location.pathname)).replace(":", " "))}.html`, "w");
 	await fs.mkdir("wiki", {recursive: true});
 	const url_file_handle = await fs.open("urls.txt", "w");
-	// while((await page.$x("//a[contains(text(), \"Next page\")]")).length > 0) {
+	let prev_url;
+	while((await page.$x("//a[contains(text(), \"Next page\")]")).length > 0) {
+		prev_url = page.url();
 		const pages = await page.$$(".mw-allpages-chunk li a");
 		let urls = [];
 		for(let c = 0; c < pages.length; c++) {
@@ -25,12 +27,14 @@ const exec = util.promisify(require('child_process').exec);
 			// console.log(page.url());
 			
 		}
-	// 	page.$x("//a[contains(text(), \"Next page\")]")[0].click();
-	// }
+		await page.goto(prev_url);
+		await page.goto((await (await page.$x("//a[contains(text(), \"Next page\")]"))[0].getProperty("href"))._remoteObject.value);
+		// await (await page.$x("//a[contains(text(), \"Next page\")]"))[0].click();
+	}
 	url_file_handle.close();
-	const {stdout, stderr} = await exec(`D: && wget --convert-links -e robots=off --adjust-extension --page-requisites --no-parent --no-clobber --restrict-file-names=windows -U \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36\" --input-file=${__dirname + "\\urls.txt"} -P ${__dirname + "\\wiki"}`);
-	console.log("stdout", stdout);
-	console.log("stderr", stderr);
+	// const {stdout, stderr} = await exec(`D: && wget --convert-links -e robots=off --adjust-extension --page-requisites --no-parent --no-clobber --restrict-file-names=windows -U \"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36\" --input-file=${__dirname + "\\urls.txt"} -P ${__dirname + "\\wiki"}`);
+	// console.log("stdout", stdout);
+	// console.log("stderr", stderr);
 	// const handle = await fs.open(__dirname + "\\wiki\\" + encodeURIComponent((await page.evaluate(() => window.location.pathname)).replace("/wiki/", "")) + ".html", "w");
 	// console.log(__dirname + "\\wiki\\" + encodeURIComponent((await page.evaluate(() => window.location.pathname)).replace("/wiki/", "")) + ".html");
 	// await handle.appendFile(await page.content());
